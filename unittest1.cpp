5cf3be6c6937a3bc7feae460a0dc455f0f315b11
kamomil <dafna3@gmail.com>
2017-06-17 14:58:02 +0300

    rgb to yuv testing - not yet working

--- unittest1.cpp
+++ unittest1.cpp
@@ -1,12 +1,19 @@
+#ifdef windows
 #include "stdafx.h"
+#endif
 //#include "CppUnitTest.h"
-#include "ipp.h"
+extern "C"
+{
+#include <ipp.h>
+#include "ippcc.h"
+}
 //#include "media_io.h"
-#include <windows.h>
+//#include <windows.h>
 //#include <random>
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 extern "C"
 {
 #include <libavcodec/avcodec.h>
@@ -29,6 +36,7 @@ uint8_t *const  	dst[],
 const int  	dstStride[]
 )
 */
+#ifdef windows
 void Output(const char* szFormat, ...)
 {
 	char szBuff[1024];
@@ -39,183 +47,176 @@ void Output(const char* szFormat, ...)
 
 	OutputDebugString(szBuff);
 }
-void print_img(uint8_t* img[], int w, int h, int c) {
+#else
+#define Output printf
+#endif
 
-	Output("Printing img %p:\n", img);
-	for (int ci = 0; ci<c; ci++) {
-		if (!img[ci]) {
-			Output("Channel %d is NULL\n", ci);
-		}
-		else {
-			Output("Channel %d:\n", ci);
-			for (int hi = 0; hi < h; hi++) {
-				for (int wi = 0; wi < w; wi++) {
-					Output("%u ", img[ci][wi + hi*w]);
-				}
-				Output("\n");
-			}
-		}
+void print_img(char* name, uint8_t* img[], int w, int h, int c) {
+
+  Output("Printing img '%s' (%p):\n",name, img);
+  for (int ci = 0; ci<c; ci++) {
+    if (!img[ci]) {
+      Output("Channel %d is NULL\n", ci);
+    }
+    else {
+      Output("Channel %d:\n", ci);
+      for (int hi = 0; hi < h; hi++) {
+	for (int wi = 0; wi < w; wi++) {
+	  Output("%u ", img[ci][wi + hi*w]);
 	}
-	return;
+	Output("\n");
+			}
+    }
+  }
+  return;
+}
+
+void l2_dist_img(unsigned int distances[], uint8_t* img1[],uint8_t* img2[], int w, int h, int c) {
+
+  for (int ci = 0; ci<c; ci++) {
+    if ( (!img1[ci] && img2[ci]) || (img1[ci] && !img2[ci]) ) {
+      Output("ERROR: channel %d: img1[ci]=%p img2[ci]=%p\n", ci,img1[ci],img2[ci]);
+      return;
+    }
+  }
+ 
+  for (int ci = 0; ci<c; ci++) {
+    if (!img[ci]) {
+      Output("Channel %d is NULL\n", ci);
+    }
+    else {
+      
+      distances[ci]=0
+      for (int hi = 0; hi < h; hi++) {
+	for (int wi = 0; wi < w; wi++) {
+	  unsigned int m1 = (unsigned int)img1[ci][wi + hi*w];
+	  unsigned int m2 = (unsigned int)img2[ci][wi + hi*w];
+	  
+	  distances[ci]+= (m1-m2)*(m1-m2);
+	}
+	Output("\n");
+			}
+    }
+  }
+  return;
 }
 
 
 void fill_img(uint8_t* img[],int w,int h,int c){
 
   for(int ci=0;ci<c;ci++){
-    for(int i=0;i<w*h;i++){
-      img[ci][i] = (uint8_t)i; 
+    if(img[ci]){
+      for(int i=0;i<w*h;i++){
+	img[ci][i] = (uint8_t)rand();
+      }
     }
   }
   return;
 }
 
-static uint64_t getSSD(uint8_t *src1, uint8_t *src2, int stride1, int stride2, int w, int h) {
-	int x, y;
-	uint64_t ssd = 0;
 
-	//printf("%d %d\n", w, h);
 
-	for (y = 0; y<h; y++) {
-		for (x = 0; x<w; x++) {
-			int d = src1[x + y*stride1] - src2[x + y*stride2];
-			ssd += d*d;
-			//printf("%d", abs(src1[x + y*stride1] - src2[x + y*stride2])/26 );
-		}
-		//printf("\n");
-	}
-	return ssd;
+void RGBtoYCbCr_ipp(unsigned char* src[3] , unsigned char *dst[] ,int height, int width)
+{
+       
+  IppiSize Roi;
+  IppiRect Rect;
+  Rect.height = Roi.height = height;
+  Rect.width = Roi.width = width;
+  Rect.x = Rect.y = 0;
+
+
+  //this is important in order to avoid a compilation error:
+  // error: invalid conversion from ‘unsigned char**’ to ‘const Ipp8u** {aka const unsigned char**}’ [-fpermissive]
+  //I think it's because src is an array so it is a const by def
+  //https://stackoverflow.com/questions/2220916/why-isnt-it-legal-to-convert-pointer-to-pointer-to-non-const-to-a-pointer-to
+  const unsigned char *source1[3];
+  source1[0] = src[0];
+  source1[1] = src[1];
+  source1[2] = src[2];
+
+  //IppStatus ippiYCbCrToRGB_<mod>(const Ipp8u* const pSrc[3], int srcStep, Ipp8u* pDst[3], int dstStep, IppiSize roiSize);
+  //this is 4:4:4 - that is no subsmapling so the 444 is ommited //see https://software.intel.com/en-us/node/503874
+  IppStatus s = ippiRGBToYCbCr_8u_P3R(source1, width,
+				      dst,
+				      width,
+				      Roi);
+  
+  if(s){
+    Output("ippiRGBToYCbCr_8u_P3R failed with error: %d\n",s);
+  }	
 }
 
-// test by ref -> src -> dst -> out & compare out against ref
-// ref & out are YV12
-static int doTest(uint8_t *ref[3], int refStride[3], int w, int h, AVPixelFormat srcFormat, AVPixelFormat dstFormat,
-	int srcW, int srcH, int dstW, int dstH, int flags) {
-	uint8_t *src[3];
-	uint8_t *dst[3];
-	uint8_t *out[3];
-	int srcStride[3], dstStride[3];
-	int i;
-	uint64_t ssdY, ssdU, ssdV;
-	struct SwsContext *srcContext, *dstContext, *outContext;
-	int res;
+int test_ippiRGBToYCbCr_8u_P3R_replacement(){
 
-	res = 0;
-	for (i = 0; i<3; i++) {
-		// avoid stride % bpp != 0
-		if (srcFormat == AV_PIX_FMT_RGB24 || srcFormat == AV_PIX_FMT_BGR24)
-			srcStride[i] = srcW * 3;
-		else
-			srcStride[i] = srcW * 4;
+  ////                                     //dst                                      //src                  
+  //void RGBtoYCbCr_ipp(unsigned char *m_pucWorkingBuffer[3], unsigned char *m_pucUnpaddedReconstruction[3] ,int height, int width)
+  
+  int w = 24;
+  int h = 24;
+  unsigned char *src[3];
+  unsigned char *dst_ipp[3];
 
-		if (dstFormat == AV_PIX_FMT_RGB24 || dstFormat == AV_PIX_FMT_BGR24)
-			dstStride[i] = dstW * 3;
-		else
-			dstStride[i] = dstW * 4;
+  src[0] = (uint8_t*)malloc(h*w);
+  src[1] = (uint8_t*)malloc(h*w);
+  src[2] = (uint8_t*)malloc(h*w);
+  
+  fill_img(src, w, h, 3);
+  print_img("src",src, w, h, 3);
+  
+  dst_ipp[0] = (uint8_t*)malloc(h*w);
+  dst_ipp[1] = (uint8_t*)malloc(h*w);
+  dst_ipp[2] = (uint8_t*)malloc(h*w);
 
-		src[i] = (uint8_t*)malloc(srcStride[i] * srcH);
-		dst[i] = (uint8_t*)malloc(dstStride[i] * dstH);
-		out[i] = (uint8_t*)malloc(refStride[i] * h);
-		if (!src[i] || !dst[i] || !out[i]) {
-			perror("Malloc");
-			res = -1;
+  memset(dst_ipp[0],0,h*w);
+  memset(dst_ipp[1],0,h*w);
+  memset(dst_ipp[2],0,h*w);
 
-			goto end;
-		}
-	}
+  ////////////////////////
+  RGBtoYCbCr_ipp(src,dst_ipp,w,h);
+  ///////////////////////
+  
+  print_img("src",src, w, h, 3);
+  print_img("dst_ipp",dst_ipp, w, h, 1);
+  
+  uint8_t* dst_ffmpeg[3];// = (uint8_t* const)malloc(3 * imgHeight*imgWidth);
+  dst_ffmpeg[0] = (uint8_t* const)malloc(h*w);
+  dst_ffmpeg[1] = (uint8_t* const)malloc(h*w);
+  dst_ffmpeg[2] = (uint8_t* const)malloc(h*w);
 
-	dstContext = outContext = NULL;
-	srcContext = sws_getContext(w, h, AV_PIX_FMT_YUV420P, srcW, srcH, srcFormat, flags, NULL, NULL, NULL);
-	if (!srcContext) {
-		Output("sws_getContext failed\n");
-		res = -1;
+  memset(dst_ffmpeg[0],0,h*w);
+  memset(dst_ffmpeg[1],0,h*w);
+  memset(dst_ffmpeg[2],0,h*w);
+    
+  int dst_ffmpeg_stride[3] = { w, w, w };
 
-		goto end;
-	}
-	dstContext = sws_getContext(srcW, srcH, srcFormat, dstW, dstH, dstFormat, flags, NULL, NULL, NULL);
-	if (!dstContext) {
-		Output("sws_getContext failed\n");
-		res = -1;
-
-		goto end;
-	}
-	outContext = sws_getContext(dstW, dstH, dstFormat, w, h, AV_PIX_FMT_YUV420P, flags, NULL, NULL, NULL);
-	if (!outContext) {
-		Output("sws_getContext failed\n");
-		res = -1;
-
-		goto end;
-	}
-	//    printf("test %X %X %X -> %X %X %X\n", (int)ref[0], (int)ref[1], (int)ref[2],
-	//        (int)src[0], (int)src[1], (int)src[2]);
-
-	sws_scale(srcContext, ref, refStride, 0, h, src, srcStride);
-	sws_scale(dstContext, src, srcStride, 0, srcH, dst, dstStride);
-	sws_scale(outContext, dst, dstStride, 0, dstH, out, refStride);
-
-	ssdY = getSSD(ref[0], out[0], refStride[0], refStride[0], w, h);
-	ssdU = getSSD(ref[1], out[1], refStride[1], refStride[1], (w + 1) >> 1, (h + 1) >> 1);
-	ssdV = getSSD(ref[2], out[2], refStride[2], refStride[2], (w + 1) >> 1, (h + 1) >> 1);
-
-	if (srcFormat == AV_PIX_FMT_GRAY8 || dstFormat == AV_PIX_FMT_GRAY8) ssdU = ssdV = 0; //FIXME check that output is really gray
-
-	ssdY /= w*h;
-	ssdU /= w*h / 4;
-	ssdV /= w*h / 4;
-
+  //AV_PIX_FMT_YUV444P - //planar YUV 4:4:4, 24bpp, (1 Cr & Cb sample per 1x1 Y samples) 
+  SwsContext * ctx = sws_getContext(w, h,AV_PIX_FMT_RGB24, w, h,AV_PIX_FMT_YUV444P, 0, NULL, NULL, NULL);
+  if (!ctx) {
+    
+    Output("ctx is null\n");
+    return 1;
+  }
+  
+  int src_strides[3] = { w,w,w}; // RGB stride
+  //
+  //int sws_scale(struct SwsContext *c, const uint8_t *const srcSlice[],
+  //const int srcStride[], int srcSliceY, int srcSliceH,
+  //uint8_t *const dst_ffmpeg[], const int dstStride[]);
+  //
+  
+  Output("sws_scale returned %d\n",sws_scale(ctx, src, src_strides, 0, h, dst_ffmpeg, dst_ffmpeg_stride));
+  print_img("dst_ffmpeg",dst_ffmpeg, w, h, 1);
 	
-
-end:
-
-	sws_freeContext(srcContext);
-	sws_freeContext(dstContext);
-	sws_freeContext(outContext);
-
-	for (i = 0; i<3; i++) {
-		free(src[i]);
-		free(dst[i]);
-		free(out[i]);
-	}
-
-	return res;
+  return 0;
 }
-
-static void selfTest(uint8_t *src[3], int stride[3], int w, int h) {
-	enum AVPixelFormat srcFormat, dstFormat;
-	int srcFormati, dstFormati;
-	int srcW, srcH, dstW, dstH;
-	int flags;
-
-	for (srcFormati = AV_PIX_FMT_NONE+1; srcFormati < AV_PIX_FMT_NB; srcFormati++) {
-		srcFormat = static_cast<AVPixelFormat>(srcFormati);
-		for (dstFormati = AV_PIX_FMT_NONE + 1; dstFormati < AV_PIX_FMT_NB; dstFormati++) {
-			
-			dstFormat = static_cast<AVPixelFormat>(dstFormati);
-			srcW = w;
-			srcH = h;
-			for (dstW = w - w / 3; dstW <= 4 * w / 3; dstW += w / 3) {
-				for (dstH = h - h / 3; dstH <= 4 * h / 3; dstH += h / 3) {
-					for (flags = 1; flags<33; flags *= 2) {
-						int res;
-
-						res = doTest(src, stride, w, h, srcFormat, dstFormat,
-							srcW, srcH, dstW, dstH, flags);
-						if (res < 0) {
-							dstW = 4 * w / 3;
-							dstH = 4 * h / 3;
-							flags = 33;
-						}
-					}
-				}
-			}
-		}
-	}
-}
-
 #define W 24
 #define H 24
 int main() {
 
+  srand(time(NULL));
+  test_ippiRGBToYCbCr_8u_P3R_replacement();
+  /*
 	int w = 3;
 	int h = 3;
 	uint8_t* rgb24Data = (uint8_t*)malloc(3 * h*w);//3 is because every pixel is reperesented by 3 bytes
@@ -240,15 +241,15 @@ int main() {
 	}
 	uint8_t * inData[1] = { rgb24Data }; // RGB24 have one plane
 	int inLinesize[1] = { 3 * w }; // RGB stride
-	/*
-	int sws_scale(struct SwsContext *c, const uint8_t *const srcSlice[],
-	const int srcStride[], int srcSliceY, int srcSliceH,
-	uint8_t *const dst[], const int dstStride[]);
-
-	*/
+	//
+	//int sws_scale(struct SwsContext *c, const uint8_t *const srcSlice[],
+	//const int srcStride[], int srcSliceY, int srcSliceH,
+	//uint8_t *const dst[], const int dstStride[]);
+	//
+	
 	sws_scale(ctx, &rgb24Data, inLinesize, 0, h, dst, dst_stride);
 	print_img(dst, w, h, 3);
-	OutputDebugString("hjghjghjg\n");
+	Output("hjghjghjg\n");
 
 	//////////////////////////////////////////////////////
 	
@@ -282,7 +283,9 @@ int main() {
 	
 
 	//selfTest(src, stride, W, H);
-	
+	/////////////////////////////////////////////////////////
 
+	*/
+  return 0;
 
 }
